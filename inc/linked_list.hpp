f9c143f8594a9538faca1cb55fd13080333c6958
Nisan <Nisan.Asraf@gmail.com>
2023-06-15 16:17:03 +0300

    added empty constructors

--- inc/linked_list.hpp
+++ inc/linked_list.hpp
@@ -6,6 +6,7 @@ namespace ds
     class Node
     {
     public:
+        Node();
         Node(const T& a_data);
         Node& operator=(const Node& a_other);
         ~Node();
@@ -15,34 +16,22 @@ namespace ds
         Node<T>* m_next;
         Node<T>* m_prev;
     };
-    
+
     template <typename T>
     class Linked_List
     {
     public:
+        Linked_List();
         Linked_List(const Linked_List& a_data);
         Linked_List& operator=(const Linked_List& a_other);
         ~Linked_List();
+
+        void Linked_List<T>::push_head(const T& a_data);
 
     private:
         Node<T>* m_head;
         Node<T>* m_tail;
     };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     template <typename T>
     Node<T>::Node(const T& a_data)
@@ -55,7 +44,7 @@ namespace ds
     template <typename T>
     Node<T>& Node<T>::operator=(const Node<T>& a_other)
     {
-        if (this != &a_other) // Check for self-assignment
+        if (this != &a_other) 
         {
             m_data = a_other.m_data;
             m_next = a_other.m_next;
@@ -75,4 +64,39 @@ namespace ds
 
     }
 
+    template <typename T>
+    Linked_List<T>::Linked_List(const Linked_List& a_data)
+    {
+
+    }
+
+    template <typename T>
+    Linked_List<T>::Linked_List()
+    {
+
+    }
+    template <typename T>
+    Linked_List<T>& Linked_List<T>::operator=(const Linked_List& a_other)
+    {
+
+    }
+
+    template <typename T>
+    void Linked_List<T>::push_head(const T& a_data)
+    {
+        Node<T>* new_node = new Node<T>(a_data);
+        if (m_head == nullptr) 
+        {
+            m_head = new_node;
+            m_prev = nullptr;
+        } else {
+            new_node->m_next = m_head;
+            m_head->m_prev = new_node;
+            m_head = new_node;
+        }
+    }
+
 } // namespace ds
+
+
+
