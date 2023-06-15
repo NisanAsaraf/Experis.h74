516383fb2bafa8f31212caaca388ede234534866
Nisan <Nisan.Asraf@gmail.com>
2023-06-15 18:10:54 +0300

    added push head mechanic

--- inc/linked_list.hpp
+++ inc/linked_list.hpp
@@ -26,7 +26,8 @@ namespace ds
         Linked_List& operator=(const Linked_List& a_other);
         ~Linked_List();
 
-        void Linked_List<T>::push_head(const T& a_data);
+        const T& Linked_List<T>::push_head(const T& a_data);
+        const T& Linked_List<T>::push_tail(const T& a_data);
 
     private:
         Node<T>* m_head;
@@ -34,10 +35,23 @@ namespace ds
     };
 
     template <typename T>
+    Node<T>::Node()
+    : m_data(nullptr)
+    , m_next(nullptr)
+    , m_prev(nullptr)
+    {  
+    }
+
+    template <typename T>
     Node<T>::Node(const T& a_data)
         : m_data(a_data)
         , m_next(nullptr)
         , m_prev(nullptr)
+    {
+    }
+
+    template <typename T>
+    Node<T>::~Node()
     {
     }
 
@@ -59,22 +73,27 @@ namespace ds
     }
 
     template <typename T>
-    Node<T>::~Node()
-    {
-
+    Linked_List<T>::Linked_List()
+    : m_head(new Node<T>())
+    , m_tail(new Node<T>())
+    {  
+        //other pointer values are defaulted to nullptr
+        m_head->m_next = m_tail;
+        m_tail->m_prev = m_head;
     }
 
     template <typename T>
     Linked_List<T>::Linked_List(const Linked_List& a_data)
+    : m_head(new Node<T>())
+    , m_tail(new Node<T>())
     {
+        Node<T>* current = a_data.m_head;
+        while(current != nullptr)
+        {
 
+        }
     }
 
-    template <typename T>
-    Linked_List<T>::Linked_List()
-    {
-
-    }
     template <typename T>
     Linked_List<T>& Linked_List<T>::operator=(const Linked_List& a_other)
     {
@@ -82,17 +101,42 @@ namespace ds
     }
 
     template <typename T>
-    void Linked_List<T>::push_head(const T& a_data)
+    const T&Linked_List<T>::push_head(const T& a_data)
     {
         Node<T>* new_node = new Node<T>(a_data);
-        if (m_head == nullptr) 
+        if(m_head->m_next == m_tail)
         {
-            m_head = new_node;
-            m_prev = nullptr;
-        } else {
+            m_tail->m_next = new_node;
+            m_head->m_prev = new_node;
+            new_node->m_next = m_head;
+            new_node->m_prev = m_tail;
+
+        }else{
+            new_node->m_prev = m_head->m_prev;
+            m_head->m_prev->m_next = new_node;
+
             new_node->m_next = m_head;
             m_head->m_prev = new_node;
-            m_head = new_node;
+        }
+    }
+
+    template <typename T>
+    const T& Linked_List<T>::push_tail(const T& a_data)
+    {
+        Node<T>* new_node = new Node<T>(a_data);
+        if(m_head->m_next == m_tail)
+        {
+            m_tail->m_next = new_node;
+            m_head->m_prev = new_node;
+            new_node->m_next = m_head;
+            new_node->m_prev = m_tail;
+
+        }else{
+            new_node->m_prev = m_head->m_prev;
+            m_head->m_prev->m_next = new_node;
+
+            new_node->m_next = m_head;
+            m_head->m_prev = new_node;
         }
     }
 
