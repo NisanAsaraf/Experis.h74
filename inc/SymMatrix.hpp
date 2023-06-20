9e5580629d8ed5edb52e31bb4aa238aa6fda06ab
Nisan <Nisan.Asraf@gmail.com>
2023-06-20 17:54:35 +0300

    added constructor implimentation

--- inc/SymMatrix.hpp
+++ inc/SymMatrix.hpp
@@ -20,7 +20,58 @@ public:
     template<typename U>
     friend std::ostream& operator<<(std::ostream &a_out, const SymMatrix<U> &a_matrix);
 
+    size_t get_dim();
+
 private:
     size_t m_dimension;
     T* m_data;
 };
+
+size_t sum_from_1_to_n(size_t n)
+{
+    return n*(n + 1)/2;
+}
+
+template <typename T>
+size_t SymMatrix<T>::get_dim()
+{
+    return m_dimension;
+}
+
+template <typename T>
+SymMatrix<T>::SymMatrix(size_t a_dim)
+:m_dimension(sum_from_1_to_n(a_dim))
+,m_data(new T[m_dimension]())
+{
+
+}
+
+template <typename T>
+SymMatrix<T>::SymMatrix(SymMatrix const& a_other)
+:m_dimension(a_other.m_dimension)
+,m_data(new T[m_dimension])
+{
+    for(size_t i = 0 ; i < m_dimension; i++)
+    {
+        this->m_data[i] = a_other.m_data[i];
+    }
+}
+
+template <typename T>
+SymMatrix<T>& SymMatrix<T>::operator=(SymMatrix const& a_other)
+{
+    if (this != &a_other) 
+    {
+        m_dimension = a_other.m_dimension;
+        T* m_safe_data = new T[m_dimension];
+
+        for(size_t i = 0 ; i < m_dimension; i++)
+        {
+           m_safe_data[i] = a_other.m_data[i];
+        }
+
+        delete[] m_data;
+        m_data = m_safe_data;
+    }
+    return *this;
+}
