97b7d3875e0283bfb9c4e04f96a9505cf7df5a1b
Nisan <Nisan.Asraf@gmail.com>
2023-06-23 19:00:58 +0300

    implemented 1-5

--- inc/generic_algorithms.hpp
+++ inc/generic_algorithms.hpp
@@ -3,70 +3,137 @@
 #include <vector>
 #include <list>
 #include <cmath>
+#include <cstring>
 
-template<typename T, typename Iter>
-T sum(Iter begin, Iter end)
+namespace pets
 {
-    T s = T{};
-    while(begin != end) 
-    {
-        s += *begin;
-        ++begin;
-    }
-    return s;
+class Cat
+{
+    public:
+    Cat();
+    Cat(const std::string& name);
+    ~Cat();
+
+    const std::string& operator*() const;
+    std::string& operator*();
+    friend std::ostream& operator<<(std::ostream& os, const Cat& cat);
+    private:
+    std::string m_name;
+};
+
+Cat::Cat(const std::string& name)
+: m_name(name)
+{
 }
 
-template<typename Iter>
-void root(Iter begin, Iter end)
+Cat::~Cat()
 {
-    while(begin != end) 
+}
+
+const std::string& Cat::operator*() const 
+{
+    return m_name;
+}
+
+std::string& Cat::operator*()
+{
+    return m_name;
+}
+
+std::ostream& operator<<(std::ostream& os, const Cat& cat)
+{
+    os<<*cat;
+    return os;
+}
+}//namespace pets
+
+template<typename Container, typename Iter = typename Container::const_iterator>
+std::ostream& operator<<(std::ostream& os, Container const& c)
+{
+    os << '[';
+    Iter it = c.begin();
+    if (it != c.end())
     {
-        *begin = sqrt(*begin);
-        ++begin;
-    }    
+        os << *it;
+        ++it;
+    }
+    while (it != c.end())
+    {
+        os << ", " << *it;
+        ++it;
+    }
+    os << "]\n";
+
+    return os;
 }
 
 template<typename Iterator>
-void print(Iterator begin, Iterator end) {    
-    while( begin != end ) 
+void print(Iterator begin, Iterator end) 
+{   
+    std::cout << '[';
+    if (begin != end)
     {
-        std::cout << *begin << ", ";
+        std::cout << *begin;
         ++begin;
     }
-    std::cout << '\n';
+    while( begin != end ) 
+    {
+        std::cout << ", "<< *begin;
+        ++begin;
+    }
+    std::cout << "]\n";
 }
 
 template<typename Container>
 void print(Container & c) 
 {
     print(c.begin(), c.end());
-
-    typename Container::iterator it = c.begin();
-    typename Container::iterator e = c.end();
-    while( it != e ) 
-    {
-        std::cout << *it << ", ";
-        ++it;
-    }
-    std::cout << '\n';
 }
 
-template<typename Container, typename T = typename Container::value_type>
-std::ostream& operator<<(std::ostream& os, Container& c)
+template<typename T = int,typename Iter>
+int mul(Iter begin, Iter end)
 {
-    size_t size = c.size();
-
-    os << "[";
-    auto it = c.begin();
-    if (it != c.end()) 
+    T mul = *begin;
+    ++begin;
+    while(begin != end) 
     {
-        os << *it;
-        ++it;
-        for (size_t i = 1; i < size; ++i, ++it) 
-        {
-            os << ", " << *it;
-        }
+        mul *= *begin;
+        ++begin;
     }
-    os << "]";
-    return os;
+    return mul;
+}
+
+template<typename T = int,typename Iter>
+int add(Iter begin, Iter end)
+{
+    T add = 0;
+    while(begin != end) 
+    {
+        add += *begin;
+        ++begin;
+    }
+    return add;
+}
+
+template<typename T = int, typename Iter>
+T foldit(Iter begin, Iter end, int (*f)(Iter, Iter))
+{
+    return f(begin, end);
+}
+
+template<typename Container>
+Container merge(Container const& a, Container const& b ,Container & c) 
+{
+    std::merge(a.begin(), a.end(), b.begin(), b.end(), c.begin());
+    std::sort(c.begin(), c.end());
+    return c;
+}
+
+template<typename Container>
+void replace_with_left_bigger(Container& v) 
+{
+    typename Container::iterator begin = v.begin();
+    typename Container::iterator end = v.end();
+    
+
 }
