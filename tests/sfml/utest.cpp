c9651d3a7927957fb4065c70793ac1d2301fff63
Nisan <Nisan.Asraf@gmail.com>
2023-07-18 00:25:38 +0300

    working in IDE , technical problems on wsl

--- tests/sfml/utest.cpp
+++ tests/sfml/utest.cpp
@@ -1,21 +1,31 @@
-#include "mu_test.h"
 #include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
 
 namespace sfml_test
 {
+
+
     class Make_window
     {
     public:
-        Make_window() : window(sf::VideoMode(800, 600), "Arkanoid", sf::Style::Titlebar | sf::Style::Close) 
+        Make_window() : window(sf::VideoMode(800, 600), "Arkanoid", sf::Style::Titlebar | sf::Style::Close)
         {
-            window.setFramerateLimit(60);
-            window.setVerticalSyncEnabled(true);
 
-            //window.setWindowStyle(sf::Style::Titlebar | sf::Style::Close);
+            float borderWidth = window.getSize().x - 10.0f ;
+            float borderHeight = window.getSize().y - 10.0f ;
 
+            border = std::make_unique<sf::RectangleShape>(sf::Vector2f(borderWidth, borderHeight));
+            border->setFillColor(sf::Color::Transparent);
 
-            window.clear(sf::Color::Blue);
+            border->setPosition(5.0f, 5.0f);
+            border->setFillColor(sf::Color::Transparent);
+            border->setOutlineThickness(5.0f);
+
+            border->setOutlineColor(sf::Color::Blue);
+
+            circle = std::make_unique<sf::CircleShape>(50.0f);
+            circle->setPosition(400.0f, 300.0f);
+            circle->setFillColor(sf::Color::Red);
+            circleVelocity = sf::Vector2f(0.05, 0.02f);
         }
 
         void run()
@@ -23,13 +33,25 @@ namespace sfml_test
             while (window.isOpen())
             {
                 processEvents();
+                circle->move(circleVelocity);
+                handleCollisions();
+                window.clear(sf::Color::Black);
+                window.draw(*border);
+                window.draw(*circle);
+                window.display(); 
             }
         }
 
-        ~Make_window() = default;
+        ~Make_window()
+        {
+           
+        }
 
     private:
         sf::RenderWindow window;
+        std::unique_ptr<sf::CircleShape> circle;
+        std::unique_ptr<sf::RectangleShape> border;
+        sf::Vector2f circleVelocity;
 
         void processEvents()
         {
@@ -39,16 +61,57 @@ namespace sfml_test
                 if (event.type == sf::Event::Closed)
                 {
                     window.close();
-                    break; 
+                    break;
                 }
             }
         }
 
+
+
+        void handleCollisions()
+        {
+            sf::Vector2f circlePosition = circle->getPosition();
+            float circleRadius = circle->getRadius();
+
+            sf::Vector2u windowSize = window.getSize();
+
+            if (circlePosition.x < 0 || circlePosition.x + 2 * circleRadius > windowSize.x)
+            {
+                circleVelocity.x = -circleVelocity.x;
+            }
+
+            if (circlePosition.y < 0 || circlePosition.y + 2 * circleRadius > windowSize.y)
+            {
+                circleVelocity.y = -circleVelocity.y;
+            }
+        }
     };
+
 } // namespace sfml_test
 
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-BEGIN_TEST(window_test)
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
+/* BEGIN_TEST(window_test)
     sfml_test::Make_window window;
     window.run();
     ASSERT_THAT(true);
@@ -57,3 +120,4 @@ END_TEST
 TEST_SUITE(決して道から外れてはいけません)
 TEST(window_test)
 END_SUITE
+ */
