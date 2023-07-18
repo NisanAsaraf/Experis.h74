ed0fc4d8b4669cb200554134924563fd07a9c9dc
nisan <nisan.asraf@gmail.com>
2023-07-18 15:41:28 +0300

    game init

--- tests/sfml/utest.cpp
+++ tests/sfml/utest.cpp
@@ -1,96 +1,211 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include <random>
+
 namespace sfml_test
 {
+using namespace sf;
 
-
-    class Make_window
+class RandomColorGenerator
+{
+public:
+    static Color getRandomColor()
     {
-    public:
-        Make_window() : window(sf::VideoMode(800, 600), "Arkanoid", sf::Style::Titlebar | sf::Style::Close)
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dis(0, 255);
+        return Color(dis(gen), dis(gen), dis(gen));
+    } 
+};
+
+class Ball
+{
+public:
+
+    Ball()
+    {
+        shape = std::make_unique<CircleShape>(20.0f);
+        shape->setPosition(400.0f, 300.0f);
+        Color randomColor = RandomColorGenerator::getRandomColor();
+        shape->setFillColor(randomColor);
+
+        std::uniform_real_distribution<float> dist(-0.1f, 0.1f);
+
+        std::random_device rd;
+        std::mt19937 gen(rd());
+
+        float randomX = dist(gen);
+        float randomY = dist(gen);
+
+        Velocity = sf::Vector2f(randomX,randomY);
+    }
+
+    CircleShape& operator*()
+    {
+        return *shape;
+    }
+
+    Vector2f& getVelocity()
+    {
+        return Velocity;
+    }
+
+    Vector2f const& getPosition()
+    {
+        return shape->getPosition();
+    }
+
+    void set_color(Color a_color)
+    {
+        shape->setFillColor(a_color);
+    }
+
+    void setPosition(Vector2f vel)
+    {
+        shape->setPosition(vel.x ,vel.y);
+    }
+
+    void setPosition(float x, float y)
+    {
+        shape->setPosition(x ,y);
+    }
+    
+    float getRadius()
+    {
+        return shape->getRadius();
+    }
+
+    ~Ball() = default;
+
+private:
+    std::unique_ptr<CircleShape> shape;
+    Vector2f Velocity;
+};
+
+class Game_Window
+{
+public:
+    Game_Window() : window(VideoMode(800, 600), "Arkanoid", sf::Style::Titlebar | sf::Style::Close)
+    {
+        
+        float borderWidth = window.getSize().x - 10.0f ;
+        float borderHeight = window.getSize().y - 10.0f ;
+        sf::Color Teal(0, 128, 128);
+
+        border = std::make_unique<sf::RectangleShape>(Vector2f(borderWidth, borderHeight));
+        border->setFillColor(Color::Transparent);
+
+        border->setPosition(5.0f, 5.0f);
+        border->setOutlineThickness(5.0f);
+        border->setOutlineColor(Teal);
+        spawn_ball();
+    
+    }
+
+    ~Game_Window() = default;
+
+    void spawn_ball()
+    {
+        balls.emplace_back(std::make_unique<Ball>());
+    }
+
+    void run()
+    {
+        while (window.isOpen())
         {
+            processEvents();
 
-            float borderWidth = window.getSize().x - 10.0f ;
-            float borderHeight = window.getSize().y - 10.0f ;
+            handleCollisions();
 
-            border = std::make_unique<sf::RectangleShape>(sf::Vector2f(borderWidth, borderHeight));
-            border->setFillColor(sf::Color::Transparent);
+            window.clear(Color::White);
+            window.draw(*border);
 
-            border->setPosition(5.0f, 5.0f);
-            border->setFillColor(sf::Color::Transparent);
-            border->setOutlineThickness(5.0f);
+            draw_shapes();
+            animate();
 
-            border->setOutlineColor(sf::Color::Blue);
-
-            circle = std::make_unique<sf::CircleShape>(50.0f);
-            circle->setPosition(400.0f, 300.0f);
-            circle->setFillColor(sf::Color::Red);
-            circleVelocity = sf::Vector2f(0.05, 0.02f);
+            window.display();
         }
+    }
 
-        void run()
+    void animate()
+    {
+        for (const auto& ballPtr : balls)
         {
-            while (window.isOpen())
+            Ball& ball = *(ballPtr.get());
+            CircleShape& shape = *ball;
+            Vector2f& velocity = ball.getVelocity();
+            shape.move(velocity);
+        }
+    }
+
+    void draw_shapes()
+    {
+        for (const auto& ballPtr : balls)
+        {
+            Ball& ball = *(ballPtr.get());
+            window.draw(*ball); 
+        }
+    }
+
+    void processEvents()
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == Event::Closed || (event.type == Event::KeyPressed && event.key.code == Keyboard::Escape))
             {
-                processEvents();
-                circle->move(circleVelocity);
-                handleCollisions();
-                window.clear(sf::Color::Black);
-                window.draw(*border);
-                window.draw(*circle);
-                window.display(); 
+                window.close();
+                break;
+            }
+            if (event.type == Event::KeyPressed && event.key.code == Keyboard::Space)
+            {
+                spawn_ball();
             }
         }
+    }
 
-        ~Make_window()
+    void handleCollisions()
+    {
+        for (const auto& ballPtr : balls)
         {
-           
-        }
-
-    private:
-        sf::RenderWindow window;
-        std::unique_ptr<sf::CircleShape> circle;
-        std::unique_ptr<sf::RectangleShape> border;
-        sf::Vector2f circleVelocity;
-
-        void processEvents()
-        {
-            sf::Event event;
-            while (window.pollEvent(event))
-            {
-                if (event.type == sf::Event::Closed)
-                {
-                    window.close();
-                    break;
-                }
-            }
-        }
-
-
-
-        void handleCollisions()
-        {
-            sf::Vector2f circlePosition = circle->getPosition();
-            float circleRadius = circle->getRadius();
+            sf::Vector2f circlePosition = ballPtr->getPosition();
+            sf::Vector2f& circleVelocity = ballPtr->getVelocity();
+            bool collision = false;
+            float circleRadius = ballPtr->getRadius();
 
             sf::Vector2u windowSize = window.getSize();
 
-            if (circlePosition.x < 0 || circlePosition.x + 2 * circleRadius > windowSize.x)
+            if (circlePosition.x <= 0 || circlePosition.x + 2.5 * circleRadius > windowSize.x)
             {
                 circleVelocity.x = -circleVelocity.x;
+                collision = true;
             }
 
-            if (circlePosition.y < 0 || circlePosition.y + 2 * circleRadius > windowSize.y)
+            if (circlePosition.y <= 0 || circlePosition.y + 2.5 * circleRadius > windowSize.y)
             {
                 circleVelocity.y = -circleVelocity.y;
+                collision = true;
+            }
+
+            if(collision)
+            {
+                Color randomColor = RandomColorGenerator::getRandomColor();
+                ballPtr->set_color(randomColor);
             }
         }
-    };
+    }
+
+private:
+    sf::RenderWindow window;
+    std::unique_ptr<RectangleShape> border;
+    std::vector<std::unique_ptr<Ball>> balls;
+};
 
 } // namespace sfml_test
 
 int main()
 {
-    sfml_test::Make_window window;
+    sfml_test::Game_Window window;
     window.run();
 
     return 0;
